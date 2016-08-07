0c9b31ee1713e3132e1f76e6e4f7db225330ede3
danieladler <weston.adler@gmail.com>
2016-08-07 09:06:51 -0700

    render hardcoded Backbone models

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 var express = require('express'),
     exphbs  = require('express-handlebars'),
     app     = express();
+    Backbone = require('backbone'),
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+var stories = []
+app.locals.stories = stories;
+
+var storyModel = Backbone.Model.extend({
+  defaults: {
+    user_id: 0,
+    tech: "",
+    comp: "",
+    story: ""
+  }
+});
+
+var story1 = new storyModel({
+  tech: "Ruby",
+  comp: "English",
+  story: "It's easy to understand."
+})
+
+var story2 = new storyModel({
+  tech: "JS lexical scoping",
+  comp: "Continent -> Country -> City",
+  story: "Two people in Paris & Berlin live in the same continent but different countries & cities"
+})
+
+stories.push(story1, story2);
 
 app.get('/', function(req, res){
   res.render('index');
